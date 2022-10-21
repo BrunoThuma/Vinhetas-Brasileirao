9a9d869a264daeb870cb65cba2698649ee502a3b
Bruno Thuma <bruno.thuma@hotmail.com>
2022-10-21 13:46:08 -0300

    first commit

---
+++ Vinhetas Brasileirao/PadsView.swift
@@ -0,0 +1,20 @@
+//
+//  PadsView.swift
+//  Vinhetas Brasileirao
+//
+//  Created by Bruno Thuma on 17/10/22.
+//
+
+import SwiftUI
+
+struct PadsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PadsView_Previews: PreviewProvider {
+    static var previews: some View {
+        PadsView()
+    }
+}
